3e6ae56b2c66e171bee9cc5d6e25cdee6ad150a0
Seu Nome <seuemail@example.com>
2025-06-13 18:13:34 -0300

    Implemnetei a classe eletronico

---
+++ src/comodo/comodo.cpp
@@ -0,0 +1,38 @@
+#include  <iostream>
+
+using namespace std;
+
+class eletroDomestico {
+    public:
+    int potencia, quantidade, diasSimulado;
+    float horasUsadas, kwh;
+    void constru_eletroDomestico(int potenciaInput = 1, int quantidaInput = 1, float horasInput = 1, float diasInput = 1 ) {
+        potencia = potenciaInput;
+        quantidade = quantidaInput;
+        horasUsadas = horasInput;
+        diasSimulado = diasInput;
+    };
+    float calcKwh() {
+        kwh = (potencia * horasUsadas * diasSimulado * quantidade)/1000 ;
+        return kwh;
+    }
+
+    int setKwh(float kwhInput) {
+        kwh = kwhInput;
+    }
+
+    float getKwh() {
+        return kwh;
+    }
+
+    int getPotencia() {
+        return potencia;
+    }
+    int getQuantidade() {
+        return quantidade;
+    }
+    float getHorasUsadas() {
+        return horasUsadas;
+    }
+
+};
