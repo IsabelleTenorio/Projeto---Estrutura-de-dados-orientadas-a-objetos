41edb0e7a5f1944940f360f7f24e66d58d2ad62c
IsabelleTenorio <isabelle.tenorio05@gmail.com>
2025-06-27 14:21:12 -0300

    feature(geladeira): criei a classe filha geladeira

---
+++ src/eletronico/geladeira/geladeira.cpp
@@ -0,0 +1,6 @@
+#include "geladeira.h"
+#include <iostream>
+using namespace std;
+
+geladeira::geladeira(int potenciaInput, int quantidadeInput, int diasInput) : eletroDomestico(potenciaInput, quantidadeInput, 24.0f, diasInput) {
+}
