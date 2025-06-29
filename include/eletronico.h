c51b774701ed64b2f5eefcd587217a2f028368fa
Wallace Gabriel <wgsl@cin.ufpe.br>
2025-06-29 14:04:18 -0300

    Implementei no calculo de kwh o sistema de Standby e organizei as classes eletronico.h e .cpp

--- include/eletronico.h
+++ include/eletronico.h
@@ -1,8 +1,6 @@
 #ifndef ELETRONICO_H
 #define ELETRONICO_H
 
-using namespace std;
-
 class eletroDomestico {
 private:
     int potencia;
@@ -10,18 +8,28 @@ private:
     int diasSimulado;
     float horasUsadas;
     float kwh;
+    bool possuiStandby;
+    float potenciaStandby;
 
 public:
-    eletroDomestico(int potenciaInput = 1, int quantidadeInput = 1, float horasInput = 1, int diasInput = 1);
+    // Construtor com valores padrão é ótimo para facilitar a criação
+    eletroDomestico(int potenciaInput = 0, int quantidadeInput = 0, float horasInput = 0.0f, int diasInput = 0, bool possuiStandbyInput = false, float potenciaStandbyInput = 0.0f);
 
+    // Função que calcula consumo em kWh
     float calcKwh();
-    void setKwh(float kwhInput);
 
+    // Getters e setters
+    void setKwh(float kwhInput);
     float getKwh() const;
     int getPotencia() const;
     int getQuantidade() const;
     float getHorasUsadas() const;
     int getDiasSimulado() const;
+
+    // Standby
+    void setStandby(bool possuiStandbyInput = false, float potenciaStandbyInput = 0.0f);
+    bool getPossuiStandby() const;
+    float getPotenciaStandby() const;
 };
 
 #endif
--- src/eletronico/eletronico.cpp
+++ src/eletronico/eletronico.cpp
@@ -1,15 +1,26 @@
 #include "eletronico.h"
 
-eletroDomestico::eletroDomestico(int potenciaInput, int quantidadeInput, float horasInput, int diasInput) {
-    potencia = potenciaInput;
-    quantidade = quantidadeInput;
-    horasUsadas = horasInput;
-    diasSimulado = diasInput;
-    kwh = 0.0;
-}
+// Construtor usando member initializer list é mais eficiente
+eletroDomestico::eletroDomestico(int potenciaInput, int quantidadeInput, float horasInput, int diasInput, bool possuiStandbyInput, float potenciaStandbyInput)
+    : potencia(potenciaInput),
+      quantidade(quantidadeInput),
+      horasUsadas(horasInput),
+      diasSimulado(diasInput),
+      possuiStandby(possuiStandbyInput),
+      potenciaStandby(potenciaStandbyInput),
+      kwh(0.0f)
+{ }
 
 float eletroDomestico::calcKwh() {
-    kwh = (potencia * horasUsadas * diasSimulado * quantidade) / 1000.0;
+    float consumoAtivo = (potencia * horasUsadas * diasSimulado * quantidade) / 1000.0f;
+    float consumoStandby = 0.0f;
+
+    if (possuiStandby){
+        float horasEmStandby = 24.0f - horasUsadas;
+        consumoStandby = (potenciaStandby * horasEmStandby * diasSimulado * quantidade) / 1000.0f;
+    }
+
+    kwh = consumoAtivo + consumoStandby;
     return kwh;
 }
 
@@ -35,4 +46,17 @@ float eletroDomestico::getHorasUsadas() const {
 
 int eletroDomestico::getDiasSimulado() const {
     return diasSimulado;
+}
+
+void eletroDomestico::setStandby(bool possuiStandbyInput, float potenciaStandbyInput){
+    possuiStandby = possuiStandbyInput;
+    potenciaStandby = potenciaStandbyInput;
+}
+
+bool eletroDomestico::getPossuiStandby() const {
+    return possuiStandby;
+}
+
+float eletroDomestico::getPotenciaStandby() const {
+    return potenciaStandby;
 }
